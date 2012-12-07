23f4f2dc97dda81e08e27ceafbe2fc6e55a509e5
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2012-12-07 20:20:45 +0100

    fixed error in managing filters

--- src/extension/wps/wps-core/src/main/java/org/geoserver/wps/raster/ChangeMatrixProcess.java
+++ src/extension/wps/wps-core/src/main/java/org/geoserver/wps/raster/ChangeMatrixProcess.java
@@ -144,7 +144,7 @@ public class ChangeMatrixProcess implements GSProcess {
         // merge filter
         params = replaceParameter(
         		params, 
-        		referenceFilter, 
+        		nowFilter, 
         		ImageMosaicFormat.FILTER);
         // merge USE_JAI_IMAGEREAD to false if needed
         params = replaceParameter(
